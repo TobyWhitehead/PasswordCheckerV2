5de9eeb1617e3b62b5ea897d39bdb7a38f4c293b
Toby Whitehead <whiteheadt99@gmail.com>
2021-08-17 16:48:46 +0100

    yeah

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -3,28 +3,29 @@ import java.util.Scanner;
 public class PasswordChecker {
 
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("enter password guess:");
-        String PasswordGuess = keyboard.nextLine();
+        String PasswordGuess;
         String SamplePassword = "banana";
-        boolean correct_status = PasswordGuess.equals(SamplePassword);
-        System.out.println(correct_status);
-        if (!correct_status) {
-            int GuessLength = PasswordGuess.length();
-            int SampleLength = SamplePassword.length();
-            String PasswordGuessLower = PasswordGuess.toLowerCase();
-            String SamplePasswordLower = SamplePassword.toLowerCase();
-            if (GuessLength != SampleLength) {
-                System.out.println("incorrect length");
-            }
-            else if (PasswordGuessLower.equals(SamplePasswordLower)) {
+        do {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("enter password guess:");
+            PasswordGuess = keyboard.nextLine();
+            boolean correct_status = PasswordGuess.equals(SamplePassword);
+            System.out.println(correct_status);
+            if (!correct_status) {
+                int GuessLength = PasswordGuess.length();
+                int SampleLength = SamplePassword.length();
+                String PasswordGuessLower = PasswordGuess.toLowerCase();
+                String SamplePasswordLower = SamplePassword.toLowerCase();
+                if (GuessLength != SampleLength) {
+                    System.out.println("incorrect length");
+                } else if (PasswordGuessLower.equals(SamplePasswordLower)) {
                     System.out.println("Upper/Lower case mismatch");
-            }
-            else {
+                } else {
                     System.out.println("incorrect characters");
+                }
             }
         }
+        while (!PasswordGuess.equals(SamplePassword));
     }
 }
-
 
